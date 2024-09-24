289a1cd8e2837c0e96a5a6c41e5bbb7a3d115308
prabh-virdi <rajavirdi2001@gmail.com>
2024-09-24 18:58:20 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const gallery = document.querySelector('.gallery');
     let isFullscreenOpen = false;
 
-    VanillaTilt.init(document.querySelector(".box"), {
+    VanillaTilt.init(document.querySelector(".gallery-container"), {
       max: 10,
       speed: 200,
       easing: "cubic-bezier(.03,.98,.52,.99)",
