f74e3f019aa39285796134be3d81a48a46c7884a
prabh-virdi <rajavirdi2001@gmail.com>
2024-09-24 17:09:54 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,14 @@ document.addEventListener('DOMContentLoaded', () => {
     const gallery = document.querySelector('.gallery');
     let isFullscreenOpen = false;
 
+    VanillaTilt.init(document.querySelector(".box"), {
+      max: 10,
+      speed: 200,
+      easing: "cubic-bezier(.03,.98,.52,.99)",
+      reverse: true,
+      glare: true,
+      "max-glare": 0.1,
+    });
     galleryItems.forEach(item => {
         item.addEventListener('click', () => {
             if (isFullscreenOpen) return;
